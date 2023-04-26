81fc40fb58ddad3d534dbc246a456cb6a59b7aa2
Richard <richardprax2003@gmail.com>
2023-04-26 22:00:20 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include "include/json/json.h"
+
+
+int main()
+{
+    std::ifstream file_input("cars.json");
+    Json::Reader reader;
+    Json::Value root;
+    reader.parse(file_input, root);
+    std::cout << root;
+    return 0;
+}
+
