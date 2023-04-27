9971ac1a182b611767cbc869e27d1943c86fc48b
m4xwlfrm <67008575+m4xwlfrm@users.noreply.github.com>
2023-04-27 09:10:09 +0200

    String Test

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main()
     std::cout << root["test"]["CarID"] << std::endl;
     std::cout << root ["test"]["Name"] << std::endl;
     std::string Car1 = "cars";
-    std::cout << root [Car1]["Name"];
+    std::cout << root [Car1]["Name"] << std::endl;
     return 0;
 }
 
